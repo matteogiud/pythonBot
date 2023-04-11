37b33aac87da6ff2b2ada0d4c566da57e7a701a4
matteogiud <matteogiud@icloud.com>
2023-04-11 12:24:12 +0200

    csvparser

---
+++ py/csv_parser.py
@@ -0,0 +1,38 @@
+
+
+def parse_list(filename, delimiter=';', no_first=0) -> list:
+    import csv
+
+    data = []
+    with open(filename, mode='r', encoding="utf8") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=delimiter)
+
+        for riga in csv_reader:
+            if no_first > 0:
+                no_first -= 1
+                continue
+            data.append(riga)
+
+        return data
+
+
+def parse_dict(filename, delimiter=';', no_first=0) -> list:
+    import csv
+
+    data = []
+    with open(filename, mode='r', encoding="utf8") as csv_file:
+        while (no_first > 0):
+            next(csv_file)
+            no_first -= 1
+            # se devo skippare la prima riga
+
+        csv_reader = csv.DictReader(csv_file, delimiter=delimiter)
+
+        for riga in csv_reader:
+            data.append(riga)
+
+        return data
+
+
+open("pippo.txt", mode="w").write(
+    str(parse_dict("py\\anagrafica_impianti_attivi.csv", no_first=1)))
