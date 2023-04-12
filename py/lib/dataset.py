15b9cab250d0d8c7bc90191c83b7328e4c9e8cbe
matteogiud <98876848+matteogiud@users.noreply.github.com>
2023-04-12 14:39:44 +0200

    Update dataset.py

--- py/lib/dataset.py
+++ py/lib/dataset.py
@@ -1,24 +1,33 @@
 import sqlite3
 
-global __conn
-__conn = None
-
-
-def get_db_instance(db_file) -> sqlite3.Connection:
+class handle_db:
     global __conn
-    if __conn == None:
-        __conn = create_connection(db_file)
-    return __conn
+    __conn = None
+
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(handle_db, cls).__new__(cls)
+        return cls.instance
+
+    def __init__(self) -> None:
+        
+
+    def get_db_instance(self, db_file) -> sqlite3.Connection:
+        global __conn
+        if __conn == None:
+            __conn = create_connection(db_file)
+        return __conn
 
 
-def create_connection(db_file) -> sqlite3.Connection:
-    try:
-        print("Creating a database connection to a SQLite3 database")
-        conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
-        return conn
-    except Exception as e:
-        print(e)
-        return None
+    def create_connection(db_file) -> sqlite3.Connection:
+        try:
+            print("Creating a database connection to a SQLite3 database")
+            conn = sqlite3.connect(db_file)
+            print(sqlite3.version)
+            return conn
+        except Exception as e:
+            print(e)
+            return None
 
-get_db_instance(f"py\lib\data\database.db")
+db = handle_db()
+db.get_db_instance(f"py\lib\data\database.db")
