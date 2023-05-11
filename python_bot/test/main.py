39b1997c91d72ab2dd3b3993fa59880198ea75fd
matteogiud <matteogiud@icloud.com>
2023-05-11 20:08:40 +0200

    reorder telegram lib

--- python_bot/test/main.py
+++ python_bot/test/main.py
@@ -3,32 +3,47 @@ from modules.telegram.telegram import *
 import time
 
 
-#start thread update db
+# start thread update db
 update_db_thread = get_thread()
 update_db_thread.start()
 
 
-
-
-#start my telegram server
+# start my telegram server
 my_telegram_token = "5783533607:AAE-4_vA8BZn5PGRjBvREylshRCyt42nRBY"
-bot=Telegram(my_telegram_token, is_async=True)
+bot = Telegram(my_telegram_token, is_async=True)
+
 
 @bot.handle_command("start")
 def handle_start_command(message: TelegramMessage):
-    text = f"Benvenuto ciccio {message.chat_id}"
-    response:TelegramResponse = TelegramResponse(message.chat_id, text)
-    
-    
+    text = f"""Benvenuto {message.user_from.username}!
+        -Invia /setFuel per impostare il carburante della tua auto"""
+
+    response: TelegramResponse = TelegramResponse(message.chat_id, text)
+
+    return response
+
+
+@bot.handle_command("setFuel")
+def handle_set_fuel_command(message: TelegramMessage):
+    text = f"""Scegli il carburante:"""
+
+    button_benzina = InlineKeyboardButton("BENZINA", callback_data='benzina')
+    button_diesel = InlineKeyboardButton("GASOLIO", callback_data='gasolio')
+    button_gpl = InlineKeyboardButton("GPL", callback_data='gpl')
+    keyboard = InlineKeyboardMarkup(
+        [[button_benzina, button_diesel, button_gpl]])
+
+    response: TelegramResponse = TelegramResponse(message.chat_id, text, reply_markup=keyboard)
+
     return response
 
 
 @bot.handle_command("help")
 def handle_start_command(message: TelegramMessage):
-    bot.send_text_message(chat_id=message.chat_id, text="Ti aiuto")
-    return 
-
+    text = """- /setFuel per impostare il carburante della tua auto"""
+    response: TelegramResponse = TelegramResponse(
+        chat_id=message.chat_id, text=text)
+    return response
 
 
 bot.start_server(debug=True)
-
--- python_bot/test/modules/telegram/telegram.py
+++ python_bot/test/modules/telegram/telegram.py
@@ -4,27 +4,27 @@ import json
 from time import sleep
 
 
+class InlineKeyboardButton:
+    def __init__(self, text, callback_data):
+        self.text = text
+        self.callback_data = callback_data
 
-class TelegramMessage:
-    def __init__(self, message: dict):
-        self.message_id = message.get("message_id")
-        self.date = message.get("date")
-        self.entities = []
 
-        entities = message.get('entities')
-        for entity in entities:
-            new_entity = TelegramEntity(entity)
-            self.entities.append(new_entity)
+class InlineKeyboardMarkup:
+    def __init__(self, inline_keyboard):
+        self.inline_keyboard = inline_keyboard
 
-        self.text = message.get("text")
-        self.user_from = TelegramUser(message.get("from"))
-        self.chat_id = message.get("chat").get("id")
+    def serialize_markup(self):
+        serialized_buttons = []
+        for row in self.inline_keyboard:
+            serialized_row = []
+            for button in row:
+                serialized_row.append({'text': button.text,
+                                       'callback_data': button.callback_data
+                                       })
+            serialized_buttons.append(serialized_row)
 
-    def has_a_command(self):
-        for entity in self.entities:
-            if entity.type == "bot_command":
-                return True
-        return False
+        return json.dumps({'inline_keyboard': serialized_buttons})
 
 
 class TelegramEntity:
@@ -42,112 +42,157 @@ class TelegramUser:
         self.language_code = user.get("language_code")
 
 
-class TelegramResponse:
-    def __init__(self, chat_id: int, text: str):
-        self.chat_id = chat_id
-        self.text = text
+class TelegramMessage:
+
+    def __init__(self) -> None:
+        pass
+
+
+class TelegramCallbackQuery:
+    def __init__(self, callback_query: dict):
+        pass
+
+
+class TelegramEntities:
+    def __init__(self) -> None:
+        pass
+
+
+class TelegramUpdate:
+    def __init__(self, update: dict):
+        self.update_id = update.get("update_id")
+        self.callback_query = TelegramCallbackQuery(update.get(
+            "callback_query")) if update.get("callback_query") else None
+        self.message = TelegramMessage(update.get(
+            "message")) if update.get("message") else None
+        self.entities = TelegramEntities(update.get(
+            "entities")) if update.get("entities") else None
+
+        entities = update.get('entities')
+        for entity in entities:
+            new_entity = TelegramEntity(entity)
+            self.entities.append(new_entity)
+
+        self.text = update.get("text")
+        self.user_from = TelegramUser(message.get("from"))
+        self.chat_id = update.get("chat").get("id")
+
+    def parse_message(self, update):
+        message = update.get('message')
+        if 'message' in update:
+            message = update['message']
+            self.message_id = message.get('message_id')
+            chat = message.get('chat')
+            if chat:
+                self.chat_id = chat.get('id')
+            self.text = message.get('text')
+        elif 'callback_query' in update:
+            callback_query = update['callback_query']
+            self.message_id = callback_query.get('message')['message_id']
+            chat = callback_query.get('message')['chat']
+            if chat:
+                self.chat_id = chat.get('id')
+            self.callback_data = callback_query.get('data')
+
+    def has_a_command(self):
+        for entity in self.entities:
+            if entity.type == "bot_command":
+                return True
+        return False
 
     def get_data_json(self):
         data = {}
         data['chat_id'] = self.chat_id
         data['text'] = self.text
-        
+        data['reply_markup'] = self.reply_markup.serialize_markup(
+        ) if self.reply_markup is not None else None
+
         return data
 
-class Telegram:
 
+class TelegramResponse:
+    def __init__(self, chat_id: int, text: str, reply_markup=None):
+        self.chat_id = chat_id
+        self.text = text
+        self.reply_markup = reply_markup
+
+
+class Telegram:
     def __init__(self, token, is_async=False):
         self.token = token
         self.url = f"https://api.telegram.org/bot{token}/"
         if is_async:
-            self.server_thread = threading.Thread(target=self.server_handle)
+            self.server_thread = threading.Thread(target=self._server_polling)
         else:
             self.server_thread = None
         self.commands_handlers = {}
         self.update_offset = None
         self.debug = False
 
-    def server_handle(self):
+    def _server_polling(self):
         while True:
             updates = self.get_updates().get("result", [])
             for update in updates:
-                message = update.get("message")
-                if message:
-                    mess = TelegramMessage(message)
-                    print(f"new message from {mess}")
-                    if mess.has_a_command() == True:
-                        command = mess.text.split()[0].replace("/", "")
-                        if command in self.commands_handlers:
-                            print(f"GET /{command}")
-                            func = self.commands_handlers[command]
-                            # eseguo la funzione in un thread
-                            threading.Thread(
-                                target=self.wrapper, args=(func, mess)).start()
+                # mess = TelegramMessage(update)
+                print("UPDATE: ", update, "\r\n")
+
+                # if mess.is_a_message() == True:
+                #     if mess.has_a_command() == True:
+                #         command = mess.text.split()[0].replace("/", "")
+                #         if command in self.commands_handlers:
+                #             print(f"GET /{command}\r\n")
+                #             func = self.commands_handlers[command]
+                #             # eseguo la funzione in un thread
+                #             threading.Thread(
+                #                 target=self.wrapper, args=(func, mess)).start()
+                #     elif mess.is_a_callback_query() == True:
+                #         print(f"GET /{callback_query['data']}\r\n")
                 self.update_offset = update["update_id"] + 1
             sleep(1)
 
     def handle_command(self, command):
+        """Add an handler for a command"""
         def decorator(func):
-
             self.commands_handlers[command] = func
             return func
         return decorator
 
-    def wrapper(self, func, message):
-        response: TelegramResponse = func(message)
-        print("response: ", response)
-        if response is not None:
-            self.send_response(response)
-
     def start_server(self, debug=False):
+        """Starts the server polling for updates, debug mode: print messages"""
         self.debug = debug
         if self.server_thread is None:
-            self.server_handle()
+            self._server_polling()
         else:
             self.server_thread.start()
 
-    def getUpdates(self, params={}, setOffset=True):
-        urlUpdate = self.url + "getUpdates"
-        try:
-            response = requests.get(urlUpdate, params)
-            response.raise_for_status()
-
-            responseJson = response.json()
-            messageList = []
-
-            offsetId = -1
-            for messaggio in responseJson["result"]:
-                messageList.append(messaggio)
-                offsetId = messaggio["update_id"]
-            offsetId += 1
-            if setOffset:
-                requests.get(urlUpdate+"getUpdates",
-                             params={"offset": offsetId})
-
-        except Exception as e:
-            messageList = None
-            print(f"Error: {e}")
-
-        return messageList
-
-    def send_text_message(self, chat_id, text=None):
-        url = self.url + "sendMessage"
-        data = {"chat_id": chat_id, "text": text}
-        response = requests.post(url, json=data)
-        return response.json()
+    # def send_text_message(self, chat_id, text=None):
+    #     url = self.url + "sendMessage"
+    #     data = {"chat_id": chat_id, "text": text}
+    #     response = requests.post(url, json=data)
+    #     return response.json()
 
     def send_response(self, response: TelegramResponse):
         url = self.url + "sendMessage"
+
         data = response.get_data_json()
         print("data: ", data)
         result = requests.post(url, json=data)
         print(result.json())
 
     def get_updates(self):
-        url = self.url + "getUpdates"
-        params = {}
-        if self.update_offset:
-            params["offset"] = self.update_offset
+        try:
+            url = self.url + "getUpdates"
+            params = {}
+            if self.update_offset:
+                params["offset"] = self.update_offset
+            response = requests.get(url, params=params)
+            response.raise_for_status()
+            return response.json()
+        except Exception as e:
+            return None
 
-        response = requests.get(url, params=params)
-        return response.json()
+    def wrapper(self, func, message):
+        response: TelegramResponse = func(message)
+        print("response: ", response)
+        if response is not None:
+            self.send_response(response)
